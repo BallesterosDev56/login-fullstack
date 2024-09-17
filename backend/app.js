558f77f943575b5ed6047a58517206387685f761
ratprogramer <lsamuelmartinezruiz43@gmail.com>
2024-09-17 18:43:09 -0500

    Register route

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import mysql from 'mysql2/promise'
+
+const app = express()
+app.use(express.json())
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "basic_crud"
+})
+
+app.post("/register", async (req, res) => {
+    let {userName, userPassword} = req.body
+    try{
+        const [result] = await db.query('INSERT INTO users (user_name,user_password) VALUES (?,?)', [userName,userPassword])
+        if(result.affectedRows == 0){
+            return res.status(500).json({success: false, error: 'Query error'})
+        }else{
+            return res.status(200).json({success: true, error: 'User created successfuly'})
+        }
+    }catch(err){
+        res.status(500).json({success: false, error: err})
+    }
+})
+
+app.listen(3000, () => {console.log('corriendo')})
