7dd352cfa42bdc8198805cdc70505ed06f32013c
ratprogramer <lsamuelmartinezruiz43@gmail.com>
2024-09-19 18:16:50 -0500

    Cors

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,12 @@ import { userRouter } from './src/routes/userRoutes.js'
 
 dotenv.config()
 const app = express()
-
-app.use(express.json())
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST']
 }))
+
+app.use(express.json())
 app.use(userRouter)
 
 app.listen(process.env.PORT, () => { console.log(`corriendo en el puerto ${process.env.PORT}`)})
